40909d83ed95f04087e7900d45ee6b15b38a3a0e
willow <shaneweth77@gmail.com>
2019-04-16 17:24:54 -0400

    added some console.logs to test mouse movement

--- assets/logic.js
+++ assets/logic.js
@@ -5,3 +5,26 @@ const context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+context.strokeStyle = "#crazy";
+context.lineJoin = "round";
+context.lineCap = "round";
+
+// only allows input on mouseclick
+let isDrawing = false;
+
+// X/Y info
+let lastX = 0;
+let lastY = 0;
+
+// function to draw
+function draw(e) {
+    // when mouse is not down, this stops the fn from running.
+    if(!isDrawing) return; 
+    
+    console.log(e);
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => isDrawing = true);
+canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseout", () => isDrawing = false);
